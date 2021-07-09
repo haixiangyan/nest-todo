6723b335f5da10057d8e457ddfe42e993f8548c4
haixiangyan <haixiang6123@gmail.com>
2021-07-09 17:34:18 +0800

    feat: 添加 swagger

--- server/src/app/app.controller.ts
+++ server/src/app/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const startSwagger = (app) => {
+  const config = new DocumentBuilder()
+    .setTitle('待办事项')
+    .setDescription('nest-todo 的 API 文档')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  startSwagger(app);
+
   await app.listen(4200);
 }
 
