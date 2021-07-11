32c81dec6f5587f65f4726ea01fbea1534f8c05a
haixiangyan <haixiang6123@gmail.com>
2021-07-11 14:20:47 +0800

    feat: 完成 todo 接口的联调

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,45 @@ import axios from 'axios';
 const baseURL = 'http://localhost:4200';
 
 const App: FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodoTitle, setNewTodoTitle] = useState<string>('');
 
   useEffect(() => {
-    axios.request<Todo[]>(({ baseURL, url: '/todo', method: 'GET' }))
-      .then(response => setTodos(response.data));
+    fetchTodos().then();
   }, []);
 
-  const addTodo = () => {
-    // 添加 todo
+  const fetchTodos = async () => {
+    setLoading(true);
+    const { data } = await axios.request<Todo[]>(({ baseURL, url: '/todo', method: 'GET' }))
+    setTodos(data);
+    setLoading(false);
+  }
+
+  const addTodo = async () => {
+    setLoading(true);
+    const newTodo: Omit<Todo, 'id'> = {
+      title: newTodoTitle,
+      status: 0
+    }
+    await axios.request<Todo>(({ baseURL, url: '/todo', method: 'POST', data: newTodo }))
+    setLoading(false);
+    await fetchTodos()
   };
+
+  const deleteTodo = async (id: number) => {
+    setLoading(true);
+    await axios.request<Todo>(({ baseURL, url: `/todo/${id}`, method: 'DELETE' }))
+    setLoading(false);
+    await fetchTodos()
+  }
 
   return (
     <div className='App'>
+      {loading && <div>Loading...</div>}
+
       <div>
-        <input placeholder='输入新待办事项' type='text' />
+        <input onChange={e => setNewTodoTitle(e.target.value)} placeholder='输入新待办事项' type='text' />
         <button onClick={addTodo}>添加</button>
       </div>
 
@@ -29,6 +52,9 @@ const App: FC = () => {
           <li key={todo.id}>
             <p>{todo.title}</p>
             <small>{todo.description}</small>
+            <div>
+              <button onClick={() => deleteTodo(todo.id)}>移除</button>
+            </div>
           </li>
         ))}
       </ul>
