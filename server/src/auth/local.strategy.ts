6085eb31e83349de56fd9e4a6ba29c8b2c7a02cb
haixiangyan <haixiang6123@gmail.com>
2021-07-11 18:20:34 +0800

    feat: 动态初始化 authService

--- server/src/auth/local.strategy.ts
+++ server/src/auth/local.strategy.ts
@@ -3,15 +3,21 @@ import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
 import { User } from '../user/entities/user.entity';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ContextIdFactory, ModuleRef } from '@nestjs/core';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(private authService: AuthService) {
-    super();
+  constructor(private moduleRef: ModuleRef) {
+    super({ passReqToCallback: true });
   }
 
-  async validate(username: string, password: string): Promise<Omit<User, 'password'>> {
-    const user = await this.authService.validateUser(username, password);
+  async validate(request: Request, username: string, password: string): Promise<Omit<User, 'password'>> {
+    const contextId = ContextIdFactory.getByRequest(request);
+
+    // 现在 authService 是一个 request-scoped provider
+    const authService = await this.moduleRef.resolve(AuthService, contextId);
+
+    const user = await authService.validateUser(username, password);
 
     if (!user) {
       throw new UnauthorizedException();
