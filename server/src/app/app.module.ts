35322bef90658250a07e0bacaf19a7668331c69c
haixiangyan <haixiang6123@gmail.com>
2021-07-13 23:02:43 +0800

    feat: 添加静态服务器

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from '../todo/todo.module';
 import { UserModule } from '../user/user.module';
 import { AuthModule } from '../auth/auth.module';
 import { UploadModule } from '../upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [AuthModule, TodoModule, UserModule, UploadModule],
+  imports: [
+    AuthModule,
+    TodoModule,
+    UserModule,
+    UploadModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload_dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
