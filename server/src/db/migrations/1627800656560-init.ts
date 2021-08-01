2aee95649540fb9ccc8b6efada20e0a069c07c0b
haixiangyan <haixiang6123@gmail.com>
2021-08-01 14:51:59 +0800

    feat: 更新 migration 文件

--- server/src/db/migrations/1627800656560-init.ts
+++ server/src/db/migrations/1627800656560-init.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class relations1627787717505 implements MigrationInterface {
-    name = 'relations1627787717505'
+export class init1627800656560 implements MigrationInterface {
+    name = 'init1627800656560'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`nest_todo\`.\`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(500) NOT NULL, \`password\` varchar(500) NOT NULL, \`email\` varchar(500) NOT NULL, \`is_admin\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`nest_todo\`.\`todo\` (\`id\` int NOT NULL AUTO_INCREMENT, \`title\` varchar(500) NOT NULL, \`description\` text NOT NULL, \`status\` int NOT NULL, \`media\` text NOT NULL, \`authorId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`nest_todo\`.\`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(500) NOT NULL, \`password\` varchar(500) NOT NULL, \`email\` varchar(500) NOT NULL, \`is_admin\` int NOT NULL DEFAULT '1', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`nest_todo\`.\`todo\` (\`id\` int NOT NULL AUTO_INCREMENT, \`title\` varchar(500) NOT NULL, \`description\` text NOT NULL, \`status\` int NOT NULL DEFAULT '0', \`media\` text NOT NULL, \`authorId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`nest_todo\`.\`todo\` ADD CONSTRAINT \`FK_c56120106977cc14f975726af07\` FOREIGN KEY (\`authorId\`) REFERENCES \`nest_todo\`.\`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
