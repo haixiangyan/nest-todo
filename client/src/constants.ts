a9c4bb6f8a9a88fa6d83e810b605d175ee6a2978
haixiangyan <haixiang6123@gmail.com>
2021-08-07 20:17:20 +0800

    feat: 更新域名

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,2 @@
-export const baseURL = '//nest-todo.yanhaixiang.cn:4200/api';
-export const wsURL = '//nest-todo.yanhaixiang.cn:4200';
+export const baseURL = '//nest-todo.thedemo.cn:4200/api';
+export const wsURL = '//nest-todo.thedemo.cn:4200';
